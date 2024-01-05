9e53a21b029b6a94219cfa712204c5fc32b360ef
Juan <juan.paez@api-ux.com>
2024-01-05 11:36:32 -0500

    pr base de prueba

--- apiLogs/v1/index.php
+++ apiLogs/v1/index.php
@@ -60,4 +60,54 @@ Flight::route('POST /frontLog/', function () {
 });
 
 
+
+Flight::route('POST /postDelivery/', function () {
+        
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+   
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+   
+    $data= Flight::request()->data->data;
+    $logType= Flight::request()->data->logType;
+
+       require_once '../../apiLogs/v1/model/modelSecurity/uuid/uuidd.php';
+        
+ 
+ 
+        $gen_uuid = new generateUuid();
+        $myuuid = $gen_uuid->guidv4();
+    
+
+        $logId = substr($myuuid, 0, 8);
+
+    
+        $conectar=conn();
+
+    
+        $query = mysqli_query($conectar, "INSERT INTO frontLogs (logId, logValue, logType) VALUES ('$logId', '$data', '$logType')");
+
+        if ($query) {
+            echo "true|¡Repartidor creado con éxito!";
+        } else {
+            // Si hay un error, imprime el mensaje de error
+            echo "false|" . mysqli_error($conectar);
+        }
+        
+    
+
+
+        // Acceder a los encabezados
+    
+        
+
+       
+
+        
+});
+
+
 Flight::start();
