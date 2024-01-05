e84d1e6c3fb851bd672dc003772d08b4ed22fcfb
Juan <juan.paez@api-ux.com>
2024-01-05 12:24:15 -0500

    pr base de prueba

--- apiLogs/v1/index.php
+++ apiLogs/v1/index.php
@@ -10,53 +10,34 @@ require 'env/domain.php';
 
 
  
-
 Flight::route('POST /frontLog/', function () {
-   
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     
-   
-    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-   
-    $data= Flight::request()->data->data;
-    $logType= Flight::request()->data->logType;
+    $data = Flight::request()->data->data;
+    $logType = Flight::request()->data->logType;
 
-//       require_once '../../apiLogs/v1/model/modelSecurity/uuid/uuidd.php';
-        
- require_once '../../apiLogs/v1/model/modelSecurity/uuid/uuidd.php';
- 
-        $gen_uuid = new generateUuid();
-        $myuuid = $gen_uuid->guidv4();
+    require_once '../../apiLogs/v1/model/modelSecurity/uuid/uuidd.php';
+    $gen_uuid = new generateUuid();
+    $myuuid = $gen_uuid->guidv4();
+    $logId = substr($myuuid, 0, 8);
+
+    $conectar = conn();
+
+    // Escapar y preparar los datos para la consulta
+    $dtadta = mysqli_real_escape_string($conectar, json_encode($data));
     
+    // Ejecutar la consulta con valores preparados para evitar inyecciones SQL
+    $result = mysqli_query($conectar, "INSERT INTO frontLogs (logId, logValue, logType) VALUES ('$logId','$dtadta','$logType')");
 
-        $logId = substr($myuuid, 0, 8);
-
-    
-        $conectar=conn();
-
-    $dtadta=json_encode($data);
-        $query = mysqli_query($conectar, "INSERT INTO frontLogs (logId, logValue, logType) VALUES ('$logId',$dtadta,'$logType')");
-
-        if ($query) {
-            echo "true|¡Repartidor creado con éxito!";
-        } else {
-            // Si hay un error, imprime el mensaje de error
-            echo "false|" . mysqli_error($conectar);
-        }
-        
-    
-
-
-        // Acceder a los encabezados
-    
-        
-      
-       
-
-        
-   
+    if ($result) {
+        echo "true|¡Repartidor creado con éxito!";
+    } else {
+        echo "false|Error en la consulta: " . mysqli_error($conectar);
+        // Puedes añadir información adicional sobre el error al log del servidor
+        error_log("Error en la consulta SQL: " . mysqli_error($conectar));
+    }
 });
 
 
