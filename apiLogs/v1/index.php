bd304bfd2dbbdc4c4a7c1467018328de7b1d6dee
Juan <juan.paez@api-ux.com>
2024-01-08 13:15:21 -0500

    pr base de prueba

--- apiLogs/v1/index.php
+++ apiLogs/v1/index.php
@@ -73,6 +73,37 @@ Flight::route('POST /middleLog/', function () {
     
 });
 
+Flight::route('POST /prueba/', function () {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+    $data = Flight::request()->data->data;
+   // $logType = Flight::request()->data->logType;
+
+    require_once '../../apiLogs/v1/model/modelSecurity/uuid/uuidd.php';
+    $gen_uuid = new generateUuid();
+    $myuuid = $gen_uuid->guidv4();
+    $logId = substr($myuuid, 0, 8);
+
+    $conectar = conn();
+
+    // Escapar y preparar los datos para la consulta
+    $dtadta = mysqli_real_escape_string($conectar, json_encode($data));
+    
+    // Ejecutar la consulta con valores preparados para evitar inyecciones SQL
+    $result = mysqli_query($conectar, "INSERT INTO prueba (valores) VALUES ('$data')");
+
+    if ($result) {
+        echo "true|Repartidor creado con éxito!";
+    } else {
+        echo "false|Error en la consulta: " . mysqli_error($conectar);
+        // Puedes añadir información adicional sobre el error al log del servidor
+        error_log("Error en la consulta SQL: " . mysqli_error($conectar));
+    }
+    
+});
+
 
 
 Flight::start();
